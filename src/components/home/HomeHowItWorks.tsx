8e17e451bf8c78d6f01845ccb3caa306de1cde7a
Dunja Kolarski <kolarskidunja96@gmail.com>
2026-07-25 02:45:08 +0200

    feat: add home how it works component and styles

---
+++ src/components/home/HomeHowItWorks.tsx
@@ -0,0 +1,83 @@
+import "./HomeHowItWorks.css";
+import restart from "../../assets/home-restart.png";
+import profile1 from "../../assets/profile1.png";
+import profile1Logo from "../../assets/profile1-logo.png";
+import profile2 from "../../assets/profile2.png";
+import profile2Logo from "../../assets/profile2-logo.png";
+import profile3 from "../../assets/profile3.png";
+import profile3Logo from "../../assets/profile3-logo.png";
+import profile4 from "../../assets/profile4.png";
+import profile4Logo from "../../assets/profile4-logo.png";
+import profile5 from "../../assets/profile5.png";
+import profile5Logo from "../../assets/profile5-logo.png";
+import profile6 from "../../assets/profile6.png";
+import profile6Logo from "../../assets/profile6-logo.png";
+import profile7 from "../../assets/profile7.png";
+import profile7Logo from "../../assets/profile7-logo.png";
+import profile8 from "../../assets/profile8.png";
+import profile8Logo from "../../assets/profile8-logo.png";
+
+function HomeHowItWorks() {
+  return (
+    <section className="home-how-does-it-work">
+      <h2>How Does it Work?</h2>
+      <div className="home-how-work-arr">
+        <div className="arr-section">
+          <div>1</div>
+          <p>Find a Coach</p>
+        </div>
+        <i className="bi bi-arrow-right"></i>
+        <div className="arr-section">
+          <div>2</div>
+          <p>Book Your Session</p>
+        </div>
+        <i className="bi bi-arrow-right"></i>
+        <div className="arr-section">
+          <div>3</div>
+          <p>Get Personalized Application Strategy</p>
+        </div>
+      </div>
+      <img src={restart} alt="Restart image" />
+      <p className="home-refund">
+        <span>Full refund</span> if you're not satisfied with your first session
+      </p>
+      <div className="home-how-work-all-images">
+        <div className="home-how-work-images">
+          <img src={profile1} alt="1 profile image" className="home-how-work-images-first"/>
+          <img src={profile1Logo} alt="1 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile2} alt="2 profile image" className="home-how-work-images-first"/>
+          <img src={profile2Logo} alt="2 profile uni logo image" className="home-how-work-images-second" />
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile3} alt="3 profile image" className="home-how-work-images-first"/>
+          <img src={profile3Logo} alt="3 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile4} alt="4 profile image" className="home-how-work-images-first"/>
+          <img src={profile4Logo} alt="4 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile5} alt="5 profile image" className="home-how-work-images-first"/>
+          <img src={profile5Logo} alt="5 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile6} alt="6 profile image" className="home-how-work-images-first"/>
+          <img src={profile6Logo} alt="6 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile7} alt="7 profile image" className="home-how-work-images-first"/>
+          <img src={profile7Logo} alt="7 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+        <div className="home-how-work-images">
+          <img src={profile8} alt="8 profile image" className="home-how-work-images-first"/>
+          <img src={profile8Logo} alt="8 profile uni logo image" className="home-how-work-images-second"/>
+        </div>
+      </div>
+      <button>Browse Consultants</button>
+    </section>
+  );
+}
+
+export default HomeHowItWorks;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/layout/Header";
 import HomeHero from "../components/home/HomeHero";
 import HomeWorkWith from "../components/home/HomeWorkWith";
+import HomeHowItWorks from "../components/home/HomeHowItWorks";
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <main>
         <HomeHero />
         <HomeWorkWith />
+        <HomeHowItWorks />
       </main>
     </>
   );
