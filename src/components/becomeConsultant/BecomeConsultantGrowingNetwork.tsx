1ac0281864cf4a73d146dc00b350399eb4720e8b
Dunja Kolarski <kolarskidunja96@gmail.com>
2026-09-08 03:19:35 +0200

    feat: add become consultant growing network component and styles

---
+++ src/components/becomeConsultant/BecomeConsultantGrowingNetwork.tsx
@@ -0,0 +1,36 @@
+import "./BecomeConsultantGrowingNetwork.css";
+import profile1 from "../../assets/profile1.png";
+import profile2 from "../../assets/profile2.png";
+import profile3 from "../../assets/profile3.png";
+import profile4 from "../../assets/profile4.png";
+import profile5 from "../../assets/profile5.png";
+import profile6 from "../../assets/profile6.png";
+import profile7 from "../../assets/profile7.png";
+import profile8 from "../../assets/profile8.png";
+import profile9 from "../../assets/profile9.png";
+import profile10 from "../../assets/profile10.png";
+import profile11 from "../../assets/profile11.png";
+
+function BecomeConsultantGrowingNetwork() {
+  return (
+    <section className="growing-network">
+      <h2>Join Our Growing Network</h2>
+      <div className="growing-network-images">
+        <img src={profile2} alt="Consultant profile" />
+        <img src={profile5} alt="Consultant profile" />
+        <img src={profile4} alt="Consultant profile" />
+        <img src={profile3} alt="Consultant profile" />
+        <img src={profile7} alt="Consultant profile" />
+        <img src={profile11} alt="Consultant profile" />
+        <img src={profile9} alt="Consultant profile" />
+        <img src={profile10} alt="Consultant profile" />
+        <img src={profile2} alt="Consultant profile" />
+        <img src={profile6} alt="Consultant profile" />
+        <img src={profile8} alt="Consultant profile" />
+        <img src={profile1} alt="Consultant profile" />
+      </div>
+    </section>
+  );
+}
+
+export default BecomeConsultantGrowingNetwork;
--- src/pages/BecomeConsultant.tsx
+++ src/pages/BecomeConsultant.tsx
@@ -1,6 +1,7 @@
 import Header from "../components/layout/Header";
 import BecomeConsultantHero from "../components/becomeConsultant/BecomeConsultantHero";
 import BecomeConsultantBeYourOwnBoss from "../components/becomeConsultant/BecomeConsultantBeYourOwnBoss";
+import BecomeConsultantGrowingNetwork from "../components/becomeConsultant/BecomeConsultantGrowingNetwork";
 import Footer from "../components/layout/Footer";
 
 function BecomeConsultant() {
@@ -11,6 +12,7 @@ function BecomeConsultant() {
       <main>
         <BecomeConsultantHero />
         <BecomeConsultantBeYourOwnBoss />
+        <BecomeConsultantGrowingNetwork />
       </main>
 
       <Footer />
